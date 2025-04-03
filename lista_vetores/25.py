743373b13ab149a15dc57b694c48ef63200b9c3f
Matheus Parizoti <134006837+MatheusParizoti@users.noreply.github.com>
2025-04-02 22:05:17 -0300

    Add files via upload

--- lista_vetores/25.py
+++ lista_vetores/25.py
@@ -1,3 +1,17 @@
 ## Enunciado: Crie uma função que retorne uma lista com os índices em que um
 ## determinado valor ocorre em uma lista.
 
+lista = [1,2,4,5,4]
+
+NovaLista = []
+
+def indices():
+    n1 = int(input("Digite um numero: "))
+    for i in range(len(lista)):
+        if n1 == lista[i]:
+            NovaLista.append(i)
+    else:
+        print("Esse numero não existe")
+
+indices()
+print(NovaLista)
